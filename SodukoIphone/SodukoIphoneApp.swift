4f6a523533de10a099e9565076248434c9c3d2c4
Michael Danylchuk <mn_danylchuk@yahoo.com>
2024-05-11 13:43:22 -0700

    Initial Commit

---
+++ SodukoIphone/SodukoIphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SodukoIphoneApp.swift
+//  SodukoIphone
+//
+//  Created by Michael Danylchuk on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SodukoIphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
